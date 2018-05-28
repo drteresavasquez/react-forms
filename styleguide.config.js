4c43948ac345c16bec4a2be8b26c51cc729a1dfd
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-05-28 15:52:27 -0500

    first commit

---
+++ styleguide.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    components: 'src/components/**/*.js',
+    // ignore: ['**/components/views/*.js'],
+    ribbon: {
+      url: 'http://github.com/drteresavasquez',
+      text: 'See App In Action'
+    },
+    template: {
+      favicon: './public/favicon.ico',
+      head: {
+        links: [
+          {
+            rel: 'stylesheet',
+            href: './node_modules/semantic-ui-css/semantic.min.css'
+          },
+          {
+            rel: 'stylesheet',
+            href: './src/styles/styleguidist.css'
+          }
+        //   {
+        //     rel: 'stylesheet',
+        //     href: './node_modules/font-awesome/css/font-awesome.min.css'
+        //   }
+        ]
+      }
+    },
+    theme: {
+        color: {
+          link: 'white',
+          linkHover: 'salmon',
+          sidebarBackground: '#000000',
+        },
+        fontFamily: {
+          base: '"Droid Sans"',
+        }, 
+        fontSize:{
+          small: 13,
+        }
+      }
+};

b752e6bcbfe47b537f5e972089b9ff7aa23af006
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-05-30 09:39:35 -0500

    code

--- src/components/contactsapp/AllContacts.js
+++ src/components/contactsapp/AllContacts.js
@@ -1,120 +1,90 @@
 import React, { Component } from 'react';
-import { Card, Button, Image } from 'semantic-ui-react'
-import { Link } from 'react-router-dom';
-// import { Route } from 'react-router-dom';
-// import GetContact from './GetContact';
+import { Card } from 'semantic-ui-react'
 
+export default class AllContacts extends Component {
 
-export default class AllContacts extends Component{
     state = {
-        contacts:[]
+        contacts:[
+            {
+              "id": 1,
+              "ownerID": 1,
+              "name": "This is a test",
+              "email": "email@email.com",
+              "phone": "615-555-5555",
+              "company": "Acme",
+              "position": "Head Honchoita",
+              "notes": "These are the notes I took on this asshole."
+            },
+            {
+              "id": 2,
+              "ownerID": 1,
+              "name": "This is a test",
+              "email": "email@email.com",
+              "phone": "615-555-5555",
+              "company": "Acme",
+              "position": "Head Honchoita",
+              "notes": "These are the notes I took on this asshole."
+            },
+            {
+              "id": 3,
+              "ownerID": 1,
+              "name": "This is a test",
+              "email": "email@email.com",
+              "phone": "615-555-5555",
+              "company": "Acme",
+              "position": "Head Honchoita",
+              "notes": "These are the notes I took on this asshole."
+            },
+            {
+              "id": 4,
+              "ownerID": 1,
+              "name": "This is a test",
+              "email": "email@email.com",
+              "phone": "615-555-5555",
+              "company": "Acme",
+              "position": "Head Honchoita",
+              "notes": "These are the notes I took on this asshole."
+            }
+          ]
     }
-
-    componentDidMount(){
-        const storedUser = sessionStorage.getItem("user");
-        
-        if(storedUser){
-            const storedUserObj = JSON.parse(storedUser);
-            this.loadContacts(storedUserObj.id)
-        }else{
-            this.loadContacts(this.props.user.id)
-        }
-        
-    }
-
-    loadContacts = (id) => {
-        fetch(`http://localhost:4000/contacts?ownerID=${id}`)
-        .then((data)=>{
-            return data.json();
-        }).then((userContacts)=>{
-            this.setState({
-                contacts: userContacts
-            })
-            console.log(userContacts)
-        })
-    }
-
-    deleteContact = (id) => {
-        return fetch(`http://localhost:4000/contacts/${id}`, {
-              method: 'DELETE',
-              headers: {
-                'Content-Type': 'application/json'
-              }
-            }).then((response) => {
-                const storedUser = sessionStorage.getItem("user");
-                const storedUserObj = JSON.parse(storedUser);
-                this.loadContacts(storedUserObj.id);
-            })
-    }
-
-    render(){
+    render () {
         return(
             <div>
-            <h2>All Contacts</h2>
-                    <ContactsList contacts={this.state.contacts} deleteContact={this.deleteContact}/>
+                <h1> All Contacts </h1>
+                <ContactsList contacts={this.state.contacts} />
             </div>
         )
     }
 }
 
-class ContactsList extends Component{
-    render(){
-        const contacts = this.props.contacts.map((contact)=>(
+class ContactsList extends Component {
+    render() {
+        const contacts = this.props.contacts.map((contact) => (
             <CardExampleHeaderCard
-                key={contact.id}
-                id={contact.id}
-                name={contact.name}
-                company={contact.company}
-                notes={contact.notes}
-                image={contact.image}
-                rating={contact.rating}
-                phone={contact.phone}
-                deleteContact={this.props.deleteContact}
+            key = {contact.id}
+            id = {contact.id}
+            name = {contact.name}
+            company = {contact.company}
+            notes = {contact.notes}
             />
         ));
         return(
             <div>
-                <Card.Group>
-                    {contacts}
-                </Card.Group>
+            {contacts}
             </div>
         )
     }
-
 }
 
 
-class CardExampleHeaderCard extends Component{
-    render(){
-        return(
-            <Card>
-                <Card.Content>
-                    <Image floated='right' size='mini' src={this.props.image} />
-                    <Card.Header>
-                    <Link to={`/contactsapp/${this.props.id}`}>{this.props.name} ({this.props.company}) </Link>
-                    </Card.Header>AllCon
-                    <Card.Meta>  
-                    <Button 
-                        size='mini' 
-                        color='red'
-                        onClick={()=>{this.props.deleteContact(this.props.id)}}
-                    >
-                        Delete Contact                       
-                    </Button>
-                    </Card.Meta>
-                    <Card.Description>
-                    <strong>{this.props.notes}</strong>
-                    </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                    <div className='ui four buttons'>
-                    <Button basic color='green' icon="call" href={`tel:${this.props.phone}`}></Button>
-                    <Button basic color='green' icon="talk" href={`sms:${this.props.phone}`}></Button>
-                    <Button basic color='blue' icon="mail" href={`mailto:${this.props.email}`}></Button>
-                    <Button basic color='orange' as={Link} to={`/contactsapp/${this.props.id}`}>Edit</Button>
-                    </div>
-                </Card.Content>
-            </Card>
-        )
-    }
-}
+const CardExampleHeaderCard = (props) => (
+  <Card.Group>
+    <Card>
+      <Card.Content>
+        <Card.Header>{props.name}</Card.Header>
+        <Card.Meta>{props.company}</Card.Meta>
+        <Card.Description>{props.notes}</Card.Description>
+      </Card.Content>
+    </Card>
+  </Card.Group>
+)
--- src/components/contactsapp/Contacts.js
+++ src/components/contactsapp/Contacts.js
@@ -2,76 +2,59 @@ import React, { Component } from 'react';
 import './main.css';
 import LoginForm from './LoginForm';
 import AllContacts from './AllContacts';
-import { Divider } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
 
 export default class Authentication extends Component{
     state = {
         authed: false,
         user: {
-            id: 1
+            // id: 1,
+            // email: '1@1.com',
+            // password: '123456789'
         }
     }
 
-    componentDidMount(){
-        const stored = sessionStorage.getItem("user");
-        if(stored){
-            const parseDB = JSON.parse(stored);
-            console.log("parseDB", parseDB);
-            this.setState({
-                authed: true,
-                user: parseDB
-            })
-        }
-    }
-
+    //create method to authenticate user
     authenticateUser = (email, password) => {
-        console.log(email, password)
-        fetch(`http://localhost:4000/users?email=${email}&&${password}`)
-        .then((data)=>{
+        fetch(`http://localhost:4000/user?email=${email}&&${password}`)
+        .then((data) => {
             return data.json();
-        }).then((userArray)=>{
+        }).then ((userArray) => {
             console.log("USER ARRAY", userArray);
-            if(userArray.length===0){
+            if (userArray.length === 0){
                 console.log("USER DOES NOT EXIST")
-            }else{
+            } else {
                 this.setState({
                     user: userArray[0],
                     authed: true
                 })
-                const userObj = JSON.stringify(userArray[0]);
-                sessionStorage.setItem('user', userObj);
             }
         })
+        
+        // if (this.state.user.email === email && this.state.user.password === password) {
+        //     this.setState({
+        //         authed: true
+        //     })
+        // }
     }
 
     isUserAuthed = () => {
-        if(this.state.authed){
-            return(
-                <div>
-                <AllContacts user={this.state.user}/>
-                </div>
+        if (this.state.authed){
+            return (
+                <AllContacts user = {this.state.user} />
             )
-        }else{
-            return(
-                <LoginForm authenticateUser={this.authenticateUser}/>
+        } else {
+            return ( 
+                <LoginForm authenticateUser = {this.authenticateUser}/>
             )
         }
     }
-
 
 
     render(){
         return(
             <div className="main">
                 <h1>Welcome to Contact App</h1>
-                <Divider section />
                 {this.isUserAuthed()}
-                <Divider section />
-
-                <Link to={`/`} className='backLink'>
-                    Back to Table of Contents
-                </Link>
             </div>
         )
     }
